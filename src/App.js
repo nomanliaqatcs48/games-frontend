a2e0b8332badb9c60acc2ffe9dfdf48a7ebc6411
Muhammad Tayyab <muhammadtayyab@MP15-muhammadT-FEI-370.local>
2022-09-20 10:45:21 +0500

    Header Footer Completed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Signup from './components/SignUp';
-
-import './App.css';
-
+import React from "react";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-     <h1>InvoChat</h1>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<Signup />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </div>
+      <div>
+          <LandingPage/>
+      </div>
   );
 }
 
--- src/components/SignUp/index.js
+++
@@ -1,5 +0,0 @@
-const Signup = () => {
-    return <h1>Hi! This is signup page</h1>;
-  };
-  
-  export default Signup;
