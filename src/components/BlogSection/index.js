8fcadf60580acc0713dafe8ddbb2ac018af921fe
Husnain.Arshad <husnain.arshad@invozone.com>
2022-10-06 13:14:47 +0500

    BlogSection On Home

--- src/components/BlogSection/index.js
+++ src/components/BlogSection/index.js
@@ -1,25 +1,62 @@
-import { Container, Box, Grid, Typography } from "@mui/material";
-import { Link, graphql } from "gatsby";
-import Blog from "../../components/Blog/Blog";
-import BlogHead from "../../components/BlogHead";
-import FreeTrialAbout from "../../components/FreeTrialAbout";
 
 import React from "react";
+import { Box, Grid, Container } from "@mui/material";
+import Blog from "../Blog/Blog";
+import FreeTrialAbout from "../FreeTrialAbout";
+import { Link, graphql } from "gatsby";
+import "../FreeTrialAbout/index.scss";
+import BlogHead from "../BlogHead";
 
-const BlogSection = () => {
-    return (
-        <>
-            <Container>
-                <Box className="BlogPage">
-                    <BlogHead />
-                    <Grid container spacing={3} sx={{ display: "flex" }}>
-                        <Typography variant="h3">Abc</Typography>
-                    </Grid>
-                </Box>
-            </Container>
-            <FreeTrialAbout />
-        </>
-    );
+const BlogSection = ({ data }) => {
+  {
+    console.log(data, "Husnain");
+  }
+  return (
+    <>
+      <Container>
+        <Box className="BlogPage">
+          <BlogHead />
+          <Grid container spacing={3} sx={{ display: "flex" }}>
+            {/* {data?.allStrapiBlog?.nodes?.map((blog, index) => {
+              return (
+                <Blog
+                //   blog={blog}
+                //   key={index}
+                  customClass={index === 0 ? true : false}
+                />
+              );
+            })} */}
+            <Blog
+                //   blog={blog}
+                //   key={index}
+                //   customClass={index === 0 ? true : false}
+                />
+          </Grid>
+        </Box>
+      </Container>
+      <FreeTrialAbout />
+    </>
+  );
 };
+export const query = graphql`
+query MyQuerys {
+    allStrapiBlog {
+      nodes {
+        Slug
+        Content
+        Title
+        id
+        Image {
+          url
+        }
+        createdAt
+        check {
+          data {
+            check
+          }
+        }
+      }
+    }
+  }`;
 
 export default BlogSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,22 @@
-import * as React from "react"
-import Layout from "../baseStructure/Layout"
-import Seo from "../components/seo"
-import LandingPage from "./LandingPage"
+import React from "react";
+import { Box, Grid, Container, Typography } from "@mui/material";
+import Layout from "../baseStructure/Layout";
+import Blog from "../components/Blog/Blog";
+import FreeTrialAbout from "../components/FreeTrialAbout";
+import { Link, graphql } from "gatsby";
+import "../components/FreeTrialAbout/index.scss";
+import BlogHead from "../components/BlogHead";
+import HeroSection from "../components/HeroSection/HeroSection";
+import SimpleSetup from "../components/SimpleSetup/SimpleSetup";
+import Plateforms from "../components/Plateforms/Plateforms";
+import SuperEasy from "../components/SuperEasy/SuperEasy";
+import GetStarted from "../components/common/GetStarted";
+import Feedback from "../components/FeedBack/FeedBack";
+import FrequentQuestions from "../components/common/FrequentQuestions";
+import FreeTrial from "../components/FreeTrial/FreeTrial";
 import '../../src/styles/common/globalStyles.module.scss'
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-// const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
 const theme = createTheme({
   typography: {
     allVariants: {
@@ -16,32 +27,55 @@ const theme = createTheme({
     },
   },
 });
-const IndexPage = ({data}) => (
-  <ThemeProvider theme={theme}>
-    <Layout>
-        <Seo title="Home" />
-        <LandingPage/>
-      </Layout>
-  </ThemeProvider>
- 
-)
 
-export default IndexPage
 
-// export const query = graphql`
-// query MyQueryZaman {
-// allStrapiBlog {
-// nodes {
-// Content
-// Slug
-// Title
-// id
-// Image {
-// url
-// }
-// createdAt
-// }
-// }
-// }
-// `
+const IndexPage = ({ data }) => {
+    console.log("DATA LOGS ======>>>", data);
+    return (
+      <ThemeProvider theme={theme}>
+        <Layout>
+            <HeroSection/>
+            <SimpleSetup/>
+            <Plateforms/>
+            <SuperEasy/>
+            <GetStarted/>
+            <Feedback/>
+            <Container>
+                <Box className="BlogPage">
+                    <BlogHead />
+                    <Grid container spacing={3} sx={{ display: "flex" }}>
+                        {data?.allStrapiBlog?.nodes?.map((blog, index) => {
+                            return <Blog blog={blog} key={index} customClass={index === 0 ? true : false} />;
+                        })}
+                    </Grid>
+                </Box>
+            </Container>
+            <FrequentQuestions/>
+            <FreeTrial/>
+        </Layout>
+        </ThemeProvider>
+    );
+};
+export const query = graphql`
+    query MyQuerystwo {
+        allStrapiBlog {
+            nodes {
+                Slug
+                Content
+                Title
+                id
+                Image {
+                    url
+                }
+                createdAt
+                check {
+                    data {
+                        check
+                    }
+                }
+            }
+        }
+    }
+`;
 
+export default IndexPage;
