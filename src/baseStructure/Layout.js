94ece1755e7bb47adf982c5e5cb5213cbe9f15bc
Zaman807 <zaman.tariq@invozone.com>
2022-10-14 10:51:27 +0500

    changes

--- src/baseStructure/Layout.js
+++ src/baseStructure/Layout.js
@@ -9,7 +9,6 @@ export default function Layout({children}) {
   return ( 
     <div className='layout'>
         <Header/>
-        {/* <Navbar/>  not functioned*/}
         <div className='content'>
             {children}
         </div>
