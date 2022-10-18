e3450b1d04e847648d764def7c98bce55b5fc6f1
Zaman807 <zaman.tariq@invozone.com>
2022-10-18 09:23:26 +0500

    rebuild

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-advanced-sitemap`,
-        // `gatsby-plugin-react-helmet`,
         `gatsby-plugin-image`,
         `gatsby-plugin-sass`,
         {
