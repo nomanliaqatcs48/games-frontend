414c746260ca277dffb9643dadedb8057a212a8a
Muhammad Tayyab <muhammad.tayyab@invozone.com>
2022-10-25 15:46:08 +0500

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     },
     plugins: [
         `gatsby-plugin-image`,
+        `gatsby-plugin-no-sourcemaps`,
         `gatsby-plugin-sass`,
         {
             resolve: `gatsby-source-filesystem`,
