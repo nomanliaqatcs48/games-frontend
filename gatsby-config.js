0e27abb73c6d7f8bd5d50053ec245f9c2f0e665f
Husnain.Arshad <husnain.arshad@invozone.com>
2023-01-31 14:25:27 +0500

    Added: Blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,7 +90,8 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/Assets/images/Invochat-Favicon.png`, // This path is relative to the root of the site.
+        icon: `src/Assets/images/Invochat-Favicon.png`,
+        // This path is relative to the root of the site.
       },
     },
     {
