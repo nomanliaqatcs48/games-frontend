b261f1599c21a9883e9fff66e6fcce8516daee73
Husnain.Arshad <husnain.arshad@invozone.com>
2023-02-21 10:24:31 +0500

    Added: GTM Tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,8 +98,8 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: "https://st-backend-invochat.invo.zone/",
-        // apiURL: "http://127.0.0.1:1337",
+        // apiURL: "https://st-backend-invochat.invo.zone/",
+        apiURL: "http://127.0.0.1:1337",
         // add access token
         accessToken:
           "d1cc1ce431884a3dac5d00b915b6afd701ae9e89aa3b7999a254a63fe00512874a34f1330015fb599678496a026abf07b5ddbc716284497c6e6d701a77a5aed5d0dacd7d82eb9f14f25e3855c21914996d9205da08f1ceebd2736e8661f23aa96be1b55328e6a87831b79b5b99d50fcc2b3854cda4715c36cc00d33769068fc2",
